f147b5fc43fe522b6818800ea8b3fbc2a2d130f0
Md Faisal Raza Khan <irkfaisal@gmail.com>
2022-09-12 12:30:16 +0530

    Add files via upload

---
+++ JsDSA/JS_MapProblems.js
@@ -0,0 +1,74 @@
+const data = [
+    {
+        name: "Faisal",
+        gender: "Male",
+        height: 6,
+        color: "Black",
+        city: "Asansol",
+    },
+    {
+        name: "Raza",
+        gender: "Male",
+        height: 5.5,
+        color: "White",
+        city: "Durgapur",
+    },
+    {
+        name: "Khan",
+        gender: "Male",
+        height: 5,
+        color: "Brown",
+        city: "Kolkata",
+    },
+    {
+        name: "Jamil",
+        gender: "Male",
+        height: 6.1,
+        color: "Fair",
+        city: "Patna",
+    },
+    {
+        name: "Adil",
+        gender: "Male",
+        height: 5.6,
+        color: "Black",
+        city: "New Delhi",
+    },
+    {
+        name: "Faizan",
+        gender: "White",
+        height: 5.9,
+        color: "Fair",
+        city: "Ranchi",
+    },
+];
+// console.log(data)
+// 1. get values from array using map method
+// const names = data.map(hello => {
+//     return (hello.name);
+// });
+// console.log(names);
+
+// const city = data.map(data => {
+//     return (data.city);
+// });
+// console.log(city);
+// const gender = data.map(data => {
+//     return (data.gender);
+// });
+// console.log(gender);
+// const color = data.map(data => {
+//     return (data.color);
+// });
+// console.log(color);
+// const height = data.map(data => {
+//     return (data.height);
+// });
+// console.log(height);
+
+// using forEach method over array object
+
+
+// const forEachName = data.forEach((data) =>  {
+//    return data.name);
+// }) 
